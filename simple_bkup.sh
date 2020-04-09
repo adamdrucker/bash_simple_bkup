b1dcf435285f766deb473912e781bc56b5639bd1
Adam Drucker <adrucker@localhost.localdomain>
2020-04-09 15:05:41 -0400

    Simple backup

---
+++ simple_bkup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Init date
+TODAY=$(date +%d-%m-%Y)
+
+# Init backup directory
+BACKUPDIR="/home/$USER/Backups"
+
+test -d $BACKUPDIR
+if [ $? == "1" ]; then
+	mkdir "/home/$USER/Backups"
+fi
+
+
+# Perform backup of user Documents
+# and /etc/passwd
+#---------------------------------
+echo "Starting backup for $(date +%A) $(date +%d-%b-%Y)..."
+echo "Backup includes:"
+sleep 1s
+echo "/home/$USER/Documents"
+sleep 1s
+echo "/etc/passwd"
+
+tar -zcf /home/$USER/Backups/"$TODAY"_backup.tar.gz /home/$USER/Documents /etc/passwd 2>/dev/null 
+
+if [ $? == "0" ]; then
+	echo "Backup successful."
+else
+	echo "Backup failed, please review."
+fi
